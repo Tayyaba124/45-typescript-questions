5f3f2fb4fc5875a4934ad01977110001abfa7cbe
Tayyaba124 <160651319+Tayyaba124@users.noreply.github.com>
2024-03-30 08:06:06 -0700

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,8 @@
+let personName:string="";
+personName=prompt("what is your name?")||"";
+if(personName !== null&&personName !== ""){
+    alert(`hello ${personName},wouid you like to learn some python today?`)
+}
+else{
+    alert("you have to fill your name!")
+}
